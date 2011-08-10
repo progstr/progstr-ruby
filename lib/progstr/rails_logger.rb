2a2412b5c82d5bb8469bfb3e46d8e5a567be44c0
Hristo Deshev <hristo@deshev.com>
2011-08-10 16:38:15 +0300

    Easier config#after_initialize rails logger setup.

--- lib/progstr/rails_logger.rb
+++ lib/progstr/rails_logger.rb
@@ -3,6 +3,12 @@ module Progstr
     def initialize
       super("Rails")
     end
+
+    def self.start(rails_config)
+      rails_config.after_initialize do
+        Rails.logger = rails_config.logger = Progstr::RailsLogger.new
+      end
+    end
   end
 end
 
