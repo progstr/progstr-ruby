32ef3f89282f256773f120f8d547c51f548a77c2
Hristo Deshev <hristo@deshev.com>
2012-02-07 13:52:20 +0200

    Read config options from environment if available.
    That should make Heroku configs easier.

--- lib/progstr/config.rb
+++ lib/progstr/config.rb
@@ -1,6 +1,15 @@
 module Progstr
   class << self
     attr_accessor :host, :path_prefix, :port, :secure, :api_key, :http_open_timeout, :http_read_timeout, :proxy_host, :proxy_port, :proxy_user, :proxy_pass, :log_debug_events
+    attr_writer :api_key, :log_debug_events
+
+    def api_key
+      @api_key || ENV['PROGSTR_API_KEY']
+    end
+
+    def log_debug_events
+      @log_debug_events || !ENV['PROGSTR_LOG_DEBUG'].nil?
+    end
 
     def host
       @host ||= "api.progstr.com"
